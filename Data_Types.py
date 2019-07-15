4a370a79cce1aa867dd4d2729b96a49ce64077f8
Aizen741 <rahulsivadas2009@gmail.com>
2019-07-15 13:30:52 +0530

    data-type2

--- Data_Types.py
+++ Data_Types.py
@@ -1,16 +1,13 @@
+
 i = 4
 d = 4.0
 s = 'HackerRank '
-a = int(input())
+t = int(input())
+a = float(input())
 c = str(input())
 
-l = (i+a)
-e = (d+d)
-f =("%s" %c)
-h = (s +" "+ f)
+print(i+t)
+print(a+a)
 
-print(l)
-print(e)
-print(h)
-
+print("%s"%s + c)
 
